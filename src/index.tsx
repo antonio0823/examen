2cebef7d986978bec01b2f163fa8fd4b0c81da58
henry <zamora7991@gmail.com>
2021-08-10 09:07:50 -0600

    refactor() destructure react y react-dom en index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './core/App';
+import { StrictMode } from 'react'
+import { render } from 'react-dom'
+import App from './core/App'
 
-ReactDOM.render(
-  <React.StrictMode>
+render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root')
 )
